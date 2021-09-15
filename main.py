0d492c0226165ca39da7336700ed0cbe5e28260c
Gabriel Scatamburlo <48013350+Gabukuro@users.noreply.github.com>
2021-09-15 00:11:17 +0000

    small fixes

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ path, count = p.solve()
 path.reverse()
 for i in path:
   Utils.printMatrix(i.matrix)
+
+print('\n\n===================================')
+print('    (☞ﾟヮﾟ)☞ Finally ☜(ﾟヮﾟ☜)    ')
+text = '\n(づ￣ ³￣)づ {:2} states are explored'
+print(text.format(count))
+print('===================================')
